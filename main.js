6f2c845ebb17d2221d99e1e6d93500299a27d9bc
svphan29 <36240410+svphan29@users.noreply.github.com>
2019-02-13 23:50:06 -0700

    add checkStatus && matchWords functions

--- main.js
+++ main.js
@@ -1,65 +1,104 @@
-window.addEventListener('load', init);
+window.addEventListener("load", init);
 
-let time = 5;
+// Levels
+const levels = {
+  easy: 5,
+  medium: 3,
+  hard: 2
+};
+
+// Change level
+const currentLevel = levels.medium;
+
+let time = currentLevel;
 let score = 0;
 let isPlaying;
 
 // DOM Elements
-const wordInput = document.querySelector('#word-input');
-const currentWord = document.querySelector('#current-word');
-const scoreDisplay = document.querySelector('#score');
-const highScoreDisplay = document.querySelector('#high-score');
-const timeDisplay = document.querySelector('#time');
-const message = document.querySelector('#message');
-const seconds = document.querySelector('#seconds');
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const highScoreDisplay = document.querySelector("#high-score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
 
 const words = [
-  'hats',
-  'river',
-  'lucky',
-  'statue',
-  'generate',
-  'stubborn',
-  'cocktail',
-  'runaway',
-  'joke',
-  'developer',
-  'establishment',
-  'hero',
-  'javascript',
-  'nutrition',
-  'revolver',
-  'echo',
-  'siblings',
-  'investigate',
-  'horrendous',
-  'symptom',
-  'laughter',
-  'magic',
-  'master',
-  'space',
-  'definition'
+  "hats",
+  "river",
+  "lucky",
+  "statue",
+  "generate",
+  "stubborn",
+  "cocktail",
+  "runaway",
+  "joke",
+  "developer",
+  "establishment",
+  "hero",
+  "javascript",
+  "nutrition",
+  "revolver",
+  "echo",
+  "siblings",
+  "investigate",
+  "horrendous",
+  "symptom",
+  "laughter",
+  "magic",
+  "master",
+  "space",
+  "definition"
 ];
 
 // Initialize game
 function init() {
+  seconds.innerHTML = currentLevel;
   showWord(words);
-  setInterval(countdown, 1000); 
+  wordInput.addEventListener("input", startMatch);
+  setInterval(countdown, 1000);
   setInterval(checkStatus, 50);
+}
+
+//Start game
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWord(words);
+    wordInput.value = "";
+    score++;
+  }
+  if (score === -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
+}
+
+// Match currentWord to wordInput
+function matchWords() {
+  if (wordInput.value === currentWord.innerHTML) {
+    message.innerHTML = "Nice!";
+    return true;
+  } else {
+    message.innerHTML = "";
+    return false;
+  }
 }
 
 //Pick & show random word
 function showWord(words) {
   const randomIndex = Math.floor(Math.random() * words.length);
-  currentWord.innerHTML = words[randomIndex]
+  currentWord.innerHTML = words[randomIndex];
 }
 
-// Countdown timer  
+// Countdown timer
 function countdown() {
   // Make sure time is does not run out
-  if(time > 0) {
+  if (time > 0) {
     time--;
-  } else if(time === 0) {
+  } else if (time === 0) {
     isPlaying = false;
   }
   timeDisplay.innerHTML = time;
@@ -67,7 +106,8 @@ function countdown() {
 
 //Check game status
 function checkStatus() {
-  if(!isPlaying && time === 0) {
-    message.innerHTML = 'Game Over!';
+  if (!isPlaying && time === 0) {
+    message.innerHTML = "Game Over!";
+    score = -1;
   }
 }
